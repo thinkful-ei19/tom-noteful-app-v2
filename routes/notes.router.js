c97c16f913108e0563bb01987960aa21eef60c09
thaborak <thaborak@gmail.com>
2018-03-15 17:59:55 -0400

    router.put notes works with tags

--- routes/notes.router.js
+++ routes/notes.router.js
@@ -46,8 +46,11 @@ router.get('/notes', (req, res, next) => {
 /* ========== GET/READ SINGLE NOTES ========== */
 router.get('/notes/:id', (req, res, next) => {
   const noteId = req.params.id;
-  knex.select('id', 'title', 'content')
+  knex.select('notes.id', 'title', 'content', 'folders.id as folder_id', 'folders.name as folderName', 'tags.id as tagId', 'tags.name as tagName')
     .from('notes')
+    .leftJoin('folders', 'notes.folder_id', 'folders.id')
+    .leftJoin('notes_tags', 'notes.id', 'notes_tags.note_id')
+    .leftJoin('tags', 'notes_tags.tag_id', 'tags.id')
     .where('notes.id', noteId)
     .debug(true)
     .then(([item]) => {
@@ -63,32 +66,58 @@ router.get('/notes/:id', (req, res, next) => {
 /* ========== PUT/UPDATE A SINGLE ITEM ========== */
 router.put('/notes/:id', (req, res, next) => {
   const noteId = req.params.id;
+
+  const tags = req.body.tags;
   /***** Never trust users - validate input *****/
   const updateObj = {};
-  const updateableFields = ['title', 'content', 'folder_id'];
+  const updateableFields = ['title', 'content'];
 
   updateableFields.forEach(field => {
     if (field in req.body) {
       updateObj[field] = req.body[field];
     }
   });
+
   /***** Never trust users - validate input *****/
   if (!updateObj.title) {
     const err = new Error('Missing `title` in request body');
     err.status = 400;
     return next(err);
   }
-  knex('notes')
-    .where({ id: noteId })
-    .update({
-      title: req.body.title,
-      content: req.body.content,
-      folder_id: req.body.folder_id
-    }, ['id', 'title', 'content', 'folder_id',])
-    .then(results => res.json(results))
-    .catch(err => next(err));
-});
 
+  knex('notes')
+    .update(updateObj)
+    .where('notes.id', noteId)
+    .returning(['notes.id'])
+    .then(() => {
+      return knex('notes_tags')
+        .where('note_id', noteId)
+        .del();
+    })
+    .then(() => {
+      const tagsInsert = tags.map(tagId => ({ note_id: noteId, tag_id: tagId }));
+      return knex.insert(tagsInsert).into('notes_tags');
+    })
+    .then(() => {
+      return knex.select('notes.id', 'title', 'content', 'folder_id', 'folders.name as folder_name', 'tags.id as tagId', 'tags.name as tagName')
+        .from('notes')
+        .leftJoin('folders', 'notes.folder_id', 'folders.id')
+        .leftJoin('notes_tags', 'notes.id', 'notes_tags.note_id')
+        .leftJoin('tags', 'tags.id', 'notes_tags.tag_id')
+        .where('notes.id', noteId);
+    })
+    .then(result => {
+      if (result) {
+        const hydrated = hydrateNotes(result);
+        res.location(`${req.originalUrl}/${hydrated.id}`).status(201).json(hydrated);
+      } else {
+        next();
+      }
+    })
+    .catch(err => next(err));
+
+
+});
 /* ========== POST/CREATE ITEM ========== */
 router.post('/notes', (req, res, next) => {
   const { title, content, folder_id, tags } = req.body; // Add `folder_id`
